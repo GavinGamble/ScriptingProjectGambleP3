fece7afb9d3147624cdb5843f4ef90abe0d2d196
GavinGamble <710145@student.dusd.net>
2024-02-06 13:48:02 -0800

    2/6/24

---
+++ ScriptingProjectGamble/Assets/Scripts/MemberHiding/HumanoidClass.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class HumanoidClass : MonoBehaviour
+{
+    //Base version of the Yell method
+    public void Yell()
+    {
+        Debug.Log("Humanoid version of the Yell() method");
+    }
+}
---
+++ ScriptingProjectGamble/Assets/Scripts/MemberHiding/WarBandClass.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class WarBandClass : MonoBehaviour
+{
+    void Start()
+    {
+        HumanoidClass human = new HumanoidClass();
+        HumanoidClass enemy = new EnemyClass();
+        HumanoidClass orc = new OrcClass();
+
+        //Notice how each Humanoid variable contains
+        //a reference to a different class in the
+        //inheritance hierarchy, yet each of them
+        //calls the Humanoid Yell() method.
+        human.Yell();
+        enemy.Yell();
+        orc.Yell();
+    }
+}
