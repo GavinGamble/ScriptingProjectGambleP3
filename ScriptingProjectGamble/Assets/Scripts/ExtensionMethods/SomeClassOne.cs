fb1363dc0719da98383d4db8cbb3050fd14d8bb2
GavinGamble <710145@student.dusd.net>
2024-02-28 13:49:25 -0800

    2/28/24

---
+++ ScriptingProjectGamble/Assets/Scripts/ExtensionMethods/SomeClassOne.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class SomeClassOne : MonoBehaviour
+{
+    void Start()
+    {
+        transform.ResetTransformation();
+    }
+}
